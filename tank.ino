585d0d1d38aeefe9c4e50fd172c0089ef6f7bb8e
school <bano.notit@mail.ru>
2018-09-25 15:25:17 +0300

    Пробуем работу моторов

--- tank.ino
+++ tank.ino
@@ -6,7 +6,7 @@
 // 4 и 7 — для направления, 5 и 6 — для скорости
 #define SPEED_L      5
 #define DIR_L        4
-#define DIR_L_I      true
+#define DIR_L_I      false
 
 #define SPEED_R      6
 #define DIR_R        7
@@ -16,10 +16,7 @@
 // ---- \настройки/ ----
 #include <SoftwareSerial.h>
 
-#define SPEED_MASK 0b01111111
-#define DIR_MASK 0b10000000
-
-char vel[2];
+char vel = 0b11111111;
 
 
 void setup() {
@@ -30,39 +27,55 @@ void setup() {
   }
 
   Serial.begin(9600);
-  while (!Serial) { ; }  // wait for serial port to connect. Needed for native USB port only
+  while (!Serial) {
+    ;  // wait for serial port to connect. Needed for native USB port only
+  }
+
+  analogWrite(SPEED_L, HIGH);
+
+  controlMotors();
 
 }
 
 void loop() {
+  delay(50);
 
-  if (Serial.available() == 2) {
-  	vel[0] = Serial.read();
-  	vel[1] = Serial.read();
+  if (Serial.available() != 2) {
+    return;
   }
 
-  while (Serial.available())
-  	Serial.read();
+
+  char val = Serial.read();
+
+  bool changed = vel != val;
+
+  vel = val;
+
+  if (changed)
+    controlMotors();
 
 }
 
+
 void controlMotors() {
 
+  // Левый мотор
   analogWrite(
-  	DIR_L, 
-  	(bool)(vel[0] & DIR_MASK) ^ DIR_L_I ?
-  	  HIGH :
-  	  LOW
+    DIR_L,
+    (vel & 0b00001000) ?
+    HIGH :
+    LOW
   );
-  analogWrite(SPEED_L, vel[0] & SPEED_MASK);
+  analogWrite(SPEED_L, (vel & 0b00001111) ? HIGH : LOW);
 
 
+  /*// Правый мотор
   analogWrite(
-  	DIR_R, 
-  	(bool)(vel[1] & DIR_MASK) ^ DIR_R_I ?
-  	  HIGH :
-  	  LOW
+    DIR_R,
+    vel[1] >> 7 ?
+    HIGH :
+    LOW
   );
-  analogWrite(SPEED_R, vel[1] & SPEED_MASK);
-
+  analogWrite(SPEED_R, vel[1] & SPEED_MASK << 1);
+*/
 }
